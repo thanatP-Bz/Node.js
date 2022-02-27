f21d5b62e0109c9412213194b5a309574f7f3a22
Thanat <bztzn512@gmail.com>
2022-02-27 18:23:31 -0500

    taskmanger

--- starter/app.js
+++ starter/app.js
@@ -2,17 +2,20 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/task");
 const connectDB = require("./DB/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 require("dotenv").config();
 
 //middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 //route
-app.get("/", (req, res) => {
-  res.send("Task Manager");
-});
 
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 5000;
 
