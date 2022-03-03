838714fc5a40a8f1113ef1449f467e0527d1869e
Thanat <bztzn512@gmail.com>
2022-03-02 19:31:16 -0500

    sortAPI

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,8 +1,14 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "product testing route" });
+  const products = await Product.find({}).sort("name price");
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "product route" });
+  const { sort } = req.query;
+  const queryObject = {};
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
